c859ef66bca23a8d76d0c719d7f91e501aeaaf01
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2019-12-06 08:53:35 +0100

    base for saveCustomRSS() - checking provided rss link. New app icon

--- src/ListScreen/index.js
+++ src/ListScreen/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Modal, Text, StyleSheet, Button, TextInput } from 'react-native';
 import { FlatList, TouchableNativeFeedback } from 'react-native-gesture-handler';
+import * as rssParser from 'react-native-rss-parser';
 
 import * as RSS_JSON from '../RSS_FEEDS.json';
 import PubItem from './PubItem';
 import Icon from 'react-native-vector-icons/Feather';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class ListScreen extends Component {
 	static navigationOptions = ({navigation}) => {
@@ -28,7 +30,8 @@ export default class ListScreen extends Component {
 		super(props);
 		this.state = {
 			RSS_PUBS: [],
-			addRSSVisible: false,
+			addRSSVisible: true,
+			error: false
 		}
 	}
 	
@@ -57,6 +60,44 @@ export default class ListScreen extends Component {
 		this.setState({addRSSVisible:false});
 	}
 
+	saveCustomRSS() {
+		let RSS_LINK = this.state.customRSSLink;
+		//checking if RSS LINK starts with http or https
+		var re = new RegExp("^(http|https)://", "i");
+		if(re.test(RSS_LINK) === false) {
+			RSS_LINK = 'http://' + RSS_LINK;
+		}
+		const RSS_NAME = this.state.customRSSName;
+		//console.log(RSS_NAME, RSS_LINK);
+		
+		//checking if user typed name and link of RSS
+		if(RSS_LINK && RSS_NAME) {
+			fetch(RSS_LINK)
+			.then((response) => response.text())
+			.then((responseData) => rssParser.parse(responseData))
+			.then((rss) => {
+				//displaying error when fetched websites is not RSS
+				if(!rss.title){
+					this.setState({error: true})
+				}
+				//saving name, link to AsyncStorage
+				else {
+					const CUSTOM_LENGTH = async () => {
+						//console.log(rss);
+						const CUSTOM_FEEDS_KEYS = await AsyncStorage.getAllKeys();
+						if(!CUSTOM_FEEDS_KEYS.includes('custom_feeds')) {
+							console.log('tutaj no');
+						}
+					}; CUSTOM_LENGTH();
+				}
+			})
+			.catch(err => {
+				//console.log(err);
+				this.setState({error: true})
+			});
+		};
+	}
+
 	render() {
 		return(
 			<View>
@@ -73,7 +114,7 @@ export default class ListScreen extends Component {
 					keyExtractor={(item, index) => index.toString()}
 					ItemSeparatorComponent = { this.FeedSep }
 				/>
-				
+
 				<Modal
 					visible={this.state.addRSSVisible}
 					animationType={'fade'}
@@ -82,7 +123,7 @@ export default class ListScreen extends Component {
 				>
 					<View style={{
 						flex: 1,
-						backgroundColor: 'rgba(180, 180, 180, 0.4)',
+						backgroundColor: 'rgba(0, 0, 0, 0.3)',
 						justifyContent:'center',
 					}}
 					>
@@ -127,12 +168,35 @@ export default class ListScreen extends Component {
 										underlineColorAndroid="transparent"
 									/>
 								</View>
-								<View style={{flexDirection: 'row', marginLeft: 'auto', marginTop: 18, marginBottom: 10}}>
+								
+								<Text style={{
+									textAlign: 'center', 
+									color: '#D8000C', 
+									width: '80%',
+									marginTop: 4,
+									fontSize: 12,
+									opacity: this.state.error === true ? 1 : 0
+								}}
+								>
+									Please check that link you've provided is a working RSS link.
+								</Text>
+								
+								<View style={{flexDirection: 'row', marginLeft: 'auto', marginTop: 8, marginBottom: 10}}>
 									<TouchableNativeFeedback style={{alignSelf: 'flex-end'}}>
-										<Text style={{fontSize: 21, fontWeight: 'bold', color: '#000', padding: 4, opacity: 0.3, marginRight: 8}}  onPress={() => this.closeModal()}>Cancel</Text>
+										<Text style={{fontSize: 21, fontWeight: 'bold', color: '#000', padding: 4, opacity: 0.3, marginRight: 8}} onPress={() => this.closeModal()}>Cancel</Text>
 									</TouchableNativeFeedback>
 									<TouchableNativeFeedback style={{alignSelf: 'flex-end'}}>
-										<Text style={{fontSize: 21, fontWeight: 'bold', color: '#0080B0', padding: 4, marginRight: 8}}>Create</Text>
+										<Text style={{
+											fontSize: 21, 
+											fontWeight: 'bold', 
+											color: '#0080B0', 
+											padding: 4, 
+											marginRight: 8
+										}} 
+											onPress={() => this.saveCustomRSS()}
+										>
+											Create
+										</Text>
 									</TouchableNativeFeedback>
 								</View>
 							</View>
