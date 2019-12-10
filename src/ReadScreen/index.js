f7fa776e73f67419e05abb5e6a2de7732910a6cf
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2019-12-10 02:07:34 +0100

    not fetching rss feeds that are disabled in settings

--- src/ReadScreen/index.js
+++ src/ReadScreen/index.js
@@ -57,66 +57,77 @@ export default class ReadScreen extends React.Component {
 	}
 
 	async componentDidMount() {
-		/*for(let i=0; i<4; i++){
+		for(let i=0; i<4; i++){
 			const R_FEED = RSS_JSON.RSS[i].feeds;
 			for(let j=0; j<R_FEED.length; j++) {
 				const R_FEED_URL = R_FEED[j].url;
 				
-				const FEED_RESPONSE = await fetch(R_FEED_URL);
-				const myJson = await FEED_RESPONSE.text();
-				const rss = await rssParser.parse(myJson)
+				const ID = 'dis' + R_FEED[j].id;
+				const DISABLE_STATE = await AsyncStorage.getItem(ID);
 				
-				for(let k=0; k<6; k++) {
-					var pubName = rss.title;
-					const R_ITEM = rss.items[k];
+				if(DISABLE_STATE === 'false') {;
+					const FEED_RESPONSE = await fetch(R_FEED_URL);
+					const myJson = await FEED_RESPONSE.text();
+					const rss = await rssParser.parse(myJson)
 					
-					let re = /[0-9]{2}:/;
-					let pub = R_ITEM.published.split(re)[0];
-					re = /[a-zA-Z], /;
-					pub = pub.split(re)[1];
-					
-					let obj = {
-						title: R_ITEM.title,
-						published: pub,
-						url: R_ITEM.links[0].url,
-						publisherName: pubName,
-						categories: R_ITEM.categories[0]
-					};
-					this.setState({
-						feed: [...this.state.feed, obj]
-					});
+					for(let k=0; k<6; k++) {
+						var pubName = rss.title;
+						const R_ITEM = rss.items[k];
+						
+						let re = /[0-9]{2}:/;
+						let pub = R_ITEM.published.split(re)[0];
+						re = /[a-zA-Z], /;
+						pub = pub.split(re)[1];
+						
+						let obj = {
+							title: R_ITEM.title,
+							published: pub,
+							url: R_ITEM.links[0].url,
+							publisherName: pubName,
+							categories: R_ITEM.categories[0]
+						};
+						this.setState({
+							feed: [...this.state.feed, obj]
+						});
+					}
 				}
 			}	
-		}*/
+		}
 
 		let custom = await AsyncStorage.getItem('custom_feeds');
+		
 		if (custom !== null) {
 			custom = JSON.parse(custom);
-			
+
 			for(let i=0; i<custom.feeds.length; i++) {
 				const R_FEED_URL = custom.feeds[i].url;
 				const CUSTOM_NAME = custom.feeds[i].category;
-				const FEED_RESPONSE = await fetch(R_FEED_URL);
-				const myJson = await FEED_RESPONSE.text();
-				const rss = await rssParser.parse(myJson)
+				const ID = 'dis' + custom.feeds[i].id;
+				const DISABLE_STATE = await AsyncStorage.getItem(ID);
+				
+				if(DISABLE_STATE === 'false') {
+					const FEED_RESPONSE = await fetch(R_FEED_URL);
+					const myJson = await FEED_RESPONSE.text();
+					const rss = await rssParser.parse(myJson)
 
-				for(let j=0; j<12; j++) {
-					var pubName = rss.title;
-					
-					const C_ITEM = rss.items[j];
-					let re = /[0-9]{2}:/;
-					let pub = C_ITEM.published.split('T')[0];
+					for(let j=0; j<12; j++) {
+						var pubName = rss.title;
+						
+						const C_ITEM = rss.items[j];
+						let re = /[0-9]{2}:/;
+						let pub = C_ITEM.published.split('T')[0];
 
-					let obj = {
-						title: C_ITEM.title,
-						published: pub,
-						url: C_ITEM.links[0].url,
-						publisherName: CUSTOM_NAME,
-						categories: C_ITEM.categories[0]
-					};
-					this.setState({
-						feed: [...this.state.feed, obj]
-					});
+						let obj = {
+							title: C_ITEM.title,
+							published: pub,
+							url: C_ITEM.links[0].url,
+							publisherName: CUSTOM_NAME,
+							categories: C_ITEM.categories[0]
+						};
+						this.setState({
+							feed: [...this.state.feed, obj]
+						});
+					}
 				}
 			}
 		}
