d277731c22001b7ab059c585b62168c9c4688c93
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2020-06-07 13:36:27 +0200

    home header, and more-popup menu with screens

---
+++ src/screens/NewCategory.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+const Home = () => {
+	return (
+		<View style={styles.HomeWrapper}>
+			<Text style={{color: "#9194A1"}}>
+				create new category here
+			</Text>
+		</View>
+	);
+	
+}; export default Home;
+
+
+const styles = StyleSheet.create({
+	HomeWrapper: { 
+		flex: 1, 
+		alignItems: 'center', 
+		justifyContent: 'center' 
+	},
+});
