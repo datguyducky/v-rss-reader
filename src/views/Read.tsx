b526ab382b0c6bae6dbfef8ddfa49cb609ec1364
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2022-12-15 16:54:57 +0100

    fix: commit files that were previously missing because they were unversioned

---
+++ src/views/Read.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+
+import { Text } from '../components/Text';
+import { Layout } from '../layouts/Layout';
+
+export const Read = () => {
+	return (
+		<Layout>
+			<Text>Feeds here...</Text>
+			<StatusBar style="auto" />
+		</Layout>
+	);
+};
