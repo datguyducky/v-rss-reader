9cf97e3e4208211b7f049bf5626dd302cedcc459
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2023-03-06 19:20:51 +0100

    feat: use active item from storage to set Header title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,19 +7,22 @@ import { Filters } from '../../drawers/Filters';
 import { Heading } from '../Heading';
 import { Icon } from '../Icon';
 import { HeaderTextWrap, HeaderWrap } from './Header.styles';
+import { useFeedsCategories } from '../../hooks/useFeedsCategories';
 
 type HeaderProps = {
 	title?: string;
 };
 
 export const Header = ({ title }: HeaderProps) => {
+	const { activeItemDetails } = useFeedsCategories();
+
 	const filtersDrawerRef = useRef<BottomSheetModal>(null);
 
 	return (
 		<>
 			<HeaderWrap>
 				<HeaderTextWrap>
-					<Heading tag="h4">{title || 'All articles'}</Heading>
+					<Heading tag="h4">{activeItemDetails?.name || title}</Heading>
 					<Heading weight={300} color="#5C5F66" tag="h6">
 						00 unread
 					</Heading>
