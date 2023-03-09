30e5ff0a943d6885b77b4138fb74a7bcf54291a8
datguysheepy <36929645+datguysheepy@users.noreply.github.com>
2023-03-09 23:11:39 +0100

    fix: remove not needed anymore layout log from ReadingStats drawer

--- src/drawers/ReadingStats.tsx
+++ src/drawers/ReadingStats.tsx
@@ -11,47 +11,45 @@ export const ReadingStats = forwardRef(
 	({ navigation }: { navigation: any }, ref: ForwardedRef<BottomSheetModal>) => {
 		return (
 			<Drawer ref={ref} snapPoints={[376]} detached bottomInset={24}>
-				<View onLayout={event => console.log(event.nativeEvent.layout)}>
-					<Text
-						fontFamily="Montserrat"
-						fontSize={18}
-						color="#228BE6"
-						weight={600}
-						mb={16}
-						textAlign="center"
-					>
-						Reading Stats
-					</Text>
+				<Text
+					fontFamily="Montserrat"
+					fontSize={18}
+					color="#228BE6"
+					weight={600}
+					mb={16}
+					textAlign="center"
+				>
+					Reading Stats
+				</Text>
 
-					<SectionWrap mb={16}>
-						<SectionTitle>Total articles read</SectionTitle>
-						<SectionCount>XX</SectionCount>
-					</SectionWrap>
+				<SectionWrap mb={16}>
+					<SectionTitle>Total articles read</SectionTitle>
+					<SectionCount>XX</SectionCount>
+				</SectionWrap>
 
-					<SectionWrap>
-						<SectionTitle>Average number of articles read per day:</SectionTitle>
-						<SectionCount>XX</SectionCount>
-					</SectionWrap>
+				<SectionWrap>
+					<SectionTitle>Average number of articles read per day:</SectionTitle>
+					<SectionCount>XX</SectionCount>
+				</SectionWrap>
 
-					<Divider my={16} />
+				<Divider my={16} />
 
-					<SectionWrap mb={16}>
-						<SectionTitle>Current reading streak (5 articles/day)</SectionTitle>
-						<SectionCount>XX</SectionCount>
-					</SectionWrap>
+				<SectionWrap mb={16}>
+					<SectionTitle>Current reading streak (5 articles/day)</SectionTitle>
+					<SectionCount>XX</SectionCount>
+				</SectionWrap>
 
-					<SectionWrap>
-						<SectionTitle>Longest reading streak (5 articles/day)</SectionTitle>
-						<SectionCount>XX</SectionCount>
-					</SectionWrap>
+				<SectionWrap>
+					<SectionTitle>Longest reading streak (5 articles/day)</SectionTitle>
+					<SectionCount>XX</SectionCount>
+				</SectionWrap>
 
-					<Divider my={16} />
+				<Divider my={16} />
 
-					<SectionWrap>
-						<SectionTitle>Total time spent reading</SectionTitle>
-						<SectionCount>XX</SectionCount>
-					</SectionWrap>
-				</View>
+				<SectionWrap>
+					<SectionTitle>Total time spent reading</SectionTitle>
+					<SectionCount>XX</SectionCount>
+				</SectionWrap>
 			</Drawer>
 		);
 	},
